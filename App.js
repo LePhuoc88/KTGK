346d70d74b279bf5934a7483ba06855bebc4a946
LePhuoc88 <127310765+LePhuoc88@users.noreply.github.com>
2024-05-09 22:05:28 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react"
-import { View, Text } from "react-native"
-import { Provider } from "react-redux"
-import { firestore } from "@react-native-firebase/firestore"
 import Root from './Src/navigations/Root'
+import { StoreProvider } from "./Src/store"
 
 const App=()=> {
     return(
-        <Root/>
+        <StoreProvider>
+            <Root/>
+        </StoreProvider>
     )
 }
 
